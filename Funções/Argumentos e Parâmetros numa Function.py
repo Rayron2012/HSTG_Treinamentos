57114f97fda975dfae974c8e8a9b20149cfb238c
Rayron Soares <100003639+Rayron2012@users.noreply.github.com>
2022-06-19 11:31:13 -0300

    Deploy Att
    Atualização do aprendizado sobre funções em python

--- Funções/Argumentos e Parâmetros numa Function.py
+++ Funções/Argumentos e Parâmetros numa Function.py
@@ -31,9 +31,9 @@ Repare que bebidas não alcóolicas começam com BSA e bebidas alcoolicas começ
 Crie um programa que analise uma lista de produtos e envie instruções para a equipe de estoque dizendo quais produtos devem ser enviados para a área de bebidas alcóolicas.
 '''
 
-def ehalcoolico(bebida):
+def ehdacategoria(bebida, cod_categoria):
     bebida = bebida.upper()
-    if "BEB" in bebida:
+    if cod_categoria in bebida:
         return True
     else:
         return False
@@ -42,5 +42,7 @@ def ehalcoolico(bebida):
 produtos = ['beb46275','TFA23962','TFA64715','TFA69555','TFA56743','BSA45510','TFA44968','CAR75448','CAR23596','CAR13490','BEB21365','BEB31623','BSA62419','BEB73344','TFA20079','BEB80694','BSA11769','BEB19495','TFA14792','TFA78043','BSA33484','BEB97471','BEB62362','TFA27311','TFA17715','BEB85146','BEB48898','BEB79496','CAR38417','TFA19947','TFA58799','CAR94811','BSA59251','BEB15385','BEB24213','BEB56262','BSA96915','CAR53454','BEB75073']
 
 for produto in produtos:
-    if ehalcoolico(produto):
+    if ehdacategoria(produto, "BEB"):
         print('Enviar {} para o setor de bebidas alcóolicas'.format(produto))
+    elif ehdacategoria(produto, "BSA"):
+        print('Enviar {} para o setor de bebidas não alcóolicas'.format(produto))
