7f1dd113cfee5e0a826047f978c22c0285015340
Rayron Soares <100003639+Rayron2012@users.noreply.github.com>
2022-06-21 23:24:57 -0300

    Deploy 21/06/2022
    Mais um pouco de treinamento de Função, esta um pouco nebuloso, mas sigo treinando

---
+++ Funções/Return com mais de uma informação.py
@@ -0,0 +1,8 @@
+def operacoes_basicas(num1, num2):
+    soma = num1 + num2
+    diferenca = num1 - num2
+    mult = num1 * num2
+    div = num1 / num2
+    return (soma, diferenca, mult, div)
+
+# Oretorno de varios valores em um return por padrão python é em tupla, pode ser feito em listas e dicionario, porém dentro de uma tupla
