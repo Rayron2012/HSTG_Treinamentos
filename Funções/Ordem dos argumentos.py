59bc000131e83bfdc0660ea52d687edc4ccb629f
Rayron Soares <100003639+Rayron2012@users.noreply.github.com>
2022-06-24 17:13:38 -0300

    Deploy 24/06/2022
    Introdução a Orientação a objeto:
    
    Como ler um objeto e metodos

---
+++ Funções/Ordem dos argumentos.py
@@ -0,0 +1,12 @@
+'''
+Ordem dos Argumentos
+Estrutura:
+Sempre os positional arguments vêm antes e depois os keywords arguments.
+Sempre os argumentos individuais vêm antes e depois os "múltiplos"
+'''
+
+# def minha_funçao(args, args1 , args2, args3 ,args4, *args, **kwargs )
+
+#  Sempre utilizar os argumentos fixos primeiro
+# Sempre Utilizar o *args em segundo pois é infinito
+#  Sempre utilizar o**kwargs por ultimo, o argumento em forma de dicionario
